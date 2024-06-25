889848d5c8ae6197ddb1ef4bc9745cba834b291c
Ganesh Sai <gr359@njit.edu>
2024-06-25 19:21:10 -0400

    This code is for items.json file

--- midterm_project.py
+++ midterm_project.py
@@ -16,6 +16,17 @@ def create_customers_json(orders):
             customers[phone] = name
     return customers
 
+def create_items_json(orders):
+#Creating the items.json file.
+    items = {}
+    for order in orders:
+        for item in order['items']:
+            item_name = item['name']
+            price = item['price']
+            if item_name not in items:
+                items[item_name] = {"price": price, "orders": 0}
+            items[item_name]["orders"] += 1
+    return items
 
 
 
